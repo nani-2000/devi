fc3f484acc7f6f12fd097e26f87f9e8cf057fbfa
UshaKumari <UshaKumari@DESKTOP-C900FMF.bbrouter>
2023-10-06 12:08:31 +0530

    13th commit

---
+++ Javproject/src/com/tnsif/Interface/InExec.java
@@ -0,0 +1,12 @@
+package com.tnsif.Interface;
+
+public class InExec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        InDemo i= new InDemo();
+        i.show();
+        i.print();
+	}
+
+}
