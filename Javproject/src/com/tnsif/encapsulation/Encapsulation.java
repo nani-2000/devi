a84400506bb449dd4243b2edc0504b5dd2f2cd91
UshaKumari <UshaKumari@DESKTOP-C900FMF.bbrouter>
2023-10-06 10:45:52 +0530

    sSECOND COMMIT

---
+++ Javproject/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	public int serialno;
+	public String name;
+	public int age;
+
+public void show()
+{
+	System.out.println("serialno: " +serialno+ "\tname:" +name+  "\tage:" +age);
+}
+
+}
---
+++ Javproject/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation obj = new Encapsulation();
+		obj.serialno=1;
+		obj.name="anu";
+		obj.age=20;
+		obj.show();
+		
+	}
+
+}
