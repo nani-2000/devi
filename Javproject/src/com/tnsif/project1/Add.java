15184d3deb315c388b04f1cda1657f941a4c3f7a
UshaKumari <UshaKumari@DESKTOP-C900FMF.bbrouter>
2023-10-06 12:32:57 +0530

    20th commit

---
+++ Javproject/src/com/tnsif/project1/Add.java
@@ -0,0 +1,13 @@
+package com.tnsif.project1;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=5;
+		int n2=22;
+		System.out.println("Sum of 10 and 15" +(n1+n2));
+
+	}
+
+}
