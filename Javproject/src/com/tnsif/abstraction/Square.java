f239d8ff040482351bb4ef550534c9075e20aef5
UshaKumari <UshaKumari@DESKTOP-C900FMF.bbrouter>
2023-10-06 11:27:47 +0530

    6th commit

---
+++ Javproject/src/com/tnsif/abstraction/Square.java
@@ -0,0 +1,32 @@
+package com.tnsif.abstraction;
+
+public class Square extends Shape{
+	private float side;
+
+	
+	public Square() {
+		super();
+		side=2.0f;
+	}
+
+
+	public Square(float side) {
+		super();
+		this.side = side;
+	}
+
+
+	@Override
+	void calArea() {
+		// TODO Auto-generated method stub
+		super.area=side*side;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Square [side=" + side + "]";
+	}
+	
+
+}
