9e8fbf272e9f71e51cb6f57b1168dc4cb6d9e992
UshaKumari <UshaKumari@DESKTOP-C900FMF.bbrouter>
2023-10-06 12:25:21 +0530

    18th commit

---
+++ Javproject/src/com/tnsif/singleinherit/SingleDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.singleinherit;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st = new Student("Anusha",343526120,"Hyderabad",9089079);
+		System.out.println(st);
+	}
+}
