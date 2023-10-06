9679791b98f5e14922b09fe50227ad400c615105
UshaKumari <UshaKumari@DESKTOP-C900FMF.bbrouter>
2023-10-06 11:59:23 +0530

    12th

---
+++ Javproject/src/com/tnsif/Interface/InDemo.java
@@ -0,0 +1,18 @@
+package com.tnsif.Interface;
+
+public class InDemo implements ChildInterface,InterfaeOne{
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("print method");
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println("show method");
+
+	}
+
+}
