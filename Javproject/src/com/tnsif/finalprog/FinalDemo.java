e417d49d98c5b8c5424ebe72a8174621eb412aa0
UshaKumari <UshaKumari@192.168.1.47>
2023-10-30 14:57:55 +0530

    fifth commit

---
+++ Javproject/src/com/tnsif/finalprog/FinalDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.finalprog;
+
+public class FinalDemo {
+	int a=20;
+    void display() {
+   	 System.out.println("the value of a is" +a);
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     FinalDemo fd=new FinalDemo();
+     fd.display();
+	
+	}
+
+}
