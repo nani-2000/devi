b0cde027e441bb888b2876bb524aba69bc86030a
UshaKumari <UshaKumari@192.168.1.47>
2023-10-30 15:32:02 +0530

    Sixteen Commit

---
+++ Javproject/src/com/tnsif/thread/CreatingThreadFirst.java
@@ -0,0 +1,16 @@
+package com.tnsif.thread;
+
+public class CreatingThreadFirst extends Thread{
+	public void run(){
+		System.out.println("Hello");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CreatingThreadFirst ob =new CreatingThreadFirst();
+		ob.start();
+
+	}
+
+}
