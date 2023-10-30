ff41892982cd0858c55bb5d663f007494338252c
UshaKumari <UshaKumari@192.168.1.47>
2023-10-30 15:18:17 +0530

    fifteen commit

---
+++ Javproject/src/com/tnsif/staticprog/StaticDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.staticprog;
+
+public class StaticDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		    Myclass1.display();//static method
+            Myclass1 o1=new Myclass1();//default constructor
+            Myclass1.display();//static block
+            System.out.println(o1);//to string method
+            
+	}
+
+}
